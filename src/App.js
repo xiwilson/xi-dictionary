0d8197efc1ec6782a688c7a091db4532d806f934
xiwilson <linzi.wilson@hotmail.com>
2025-03-29 20:38:09 +0000

    Added Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello Linzi</header>
+      <header className="App-header"></header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>Coded by Linzi Wilson</footer>
     </div>
   );
 }

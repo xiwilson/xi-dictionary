43440992497676d0157c6595a294001b1a274b88
xiwilson <linzi.wilson@hotmail.com>
2025-03-29 21:01:32 +0000

    Added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {}
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition...`);
+
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=&key=${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {

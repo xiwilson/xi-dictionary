8e184c95bceff9986b2d51fcddc60af804ecc050
xiwilson <linzi.wilson@hotmail.com>
2025-03-30 17:58:57 +0100

    Second attempt to fix error
    three builds failed to upload so far

--- src/Results.js
+++ src/Results.js
@@ -7,7 +7,7 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        <p>{props.results.phonetic}</p>
+        <Phonetic phonetic={props.results.phonetic} />
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>

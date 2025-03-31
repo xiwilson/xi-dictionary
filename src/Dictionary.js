ea6e04386913ec9be1a407e8e405bff5850c0fd9
xiwilson <linzi.wilson@hotmail.com>
2025-03-31 11:46:35 +0100

    Added additional CSS

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,7 +4,7 @@ import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState("happy");
+  let [keyword, setKeyword] = useState("rainbow");
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
 
